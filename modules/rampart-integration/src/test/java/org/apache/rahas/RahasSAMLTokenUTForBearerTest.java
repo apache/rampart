2022c10acd7ac37ed6b95bc3e595688729871784
Nandana Mihindukulasooriya <nandana@apache.org>
2008-01-28 09:12:24 +0000

    rampart-policy related changes to support WS Security Policy 1.2
    git-svn-id: https://svn.apache.org/repos/asf/webservices/rampart/trunk/java@615785 13f79535-47bb-0310-9956-ffa450edef68

--- modules/rampart-integration/src/test/java/org/apache/rahas/RahasSAMLTokenUTForBearerTest.java
+++ modules/rampart-integration/src/test/java/org/apache/rahas/RahasSAMLTokenUTForBearerTest.java
@@ -22,7 +22,8 @@ import org.apache.axiom.om.OMFactory;
 import org.apache.neethi.Policy;
 import org.apache.rampart.handler.config.InflowConfiguration;
 import org.apache.rampart.handler.config.OutflowConfiguration;
-import org.apache.ws.secpolicy.Constants;
+import org.apache.ws.secpolicy.SP11Constants;
+import org.apache.ws.secpolicy.SPConstants;
 import org.opensaml.XML;
 
 import javax.xml.namespace.QName;
@@ -109,7 +110,7 @@ public class RahasSAMLTokenUTForBearerTest extends TestClient {
      */
     public OMElement getRSTTemplate() throws TrustException {
         OMFactory factory = OMAbstractFactory.getOMFactory();
-        OMElement elem = factory.createOMElement(Constants.RST_TEMPLATE.getLocalPart(), factory.createOMNamespace(Constants.RST_TEMPLATE.getNamespaceURI(),"wsp"));
+        OMElement elem = factory.createOMElement(SP11Constants.REQUEST_SECURITY_TOKEN_TEMPLATE);
         
         TrustUtil.createTokenTypeElement(RahasConstants.VERSION_05_02, elem).setText(RahasConstants.TOK_TYPE_SAML_10);
         TrustUtil.createKeyTypeElement(RahasConstants.VERSION_05_02, elem, RahasConstants.KEY_TYPE_BEARER);
