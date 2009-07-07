49d9351e98ac6ec4904d4cb9e00d56ced8fe7337
Nandana Mihindukulasooriya <nandana@apache.org>
2009-07-07 11:03:03 +0000

    adding a test case for RAMPART-232
    git-svn-id: https://svn.apache.org/repos/asf/webservices/rampart/trunk/java@791792 13f79535-47bb-0310-9956-ffa450edef68

--- modules/rampart-integration/src/test/java/org/apache/rampart/RampartTest.java
+++ modules/rampart-integration/src/test/java/org/apache/rampart/RampartTest.java
@@ -74,7 +74,7 @@ public class RampartTest extends TestCase {
                         "Unlimited Strength Jurisdiction Policy !!!");
             }
             
-            for (int i = 1; i <= 22; i++) { //<-The number of tests we have
+            for (int i = 1; i <= 23; i++) { //<-The number of tests we have
                 if(!basic256Supported && (i == 3 || i == 4 || i == 5)) {
                     //Skip the Basic256 tests
                     continue;
