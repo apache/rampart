9b427eae7e62508c78957fa436cd4ecbf59ba3a1
Andreas Veithen <veithen@apache.org>
2018-08-06 19:16:48 +0000

    Fix test failure caused by expired certificate.
    git-svn-id: https://svn.apache.org/repos/asf/axis/axis2/java/rampart/trunk@1837550 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/rampart-tests/src/test/java/org/apache/rampart/FakeValidationDateAspect.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.rampart;
+
+import java.security.cert.CertificateExpiredException;
+import java.security.cert.CertificateNotYetValidException;
+import java.security.cert.X509Certificate;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * Aspect that redirects {@link X509Certificate#checkValidity()} to
+ * {@link X509Certificate#checkValidity(Date)} with a fixed date. This allows
+ * executing unit tests relying on certificates that have expired.
+ */
+@Aspect
+public class FakeValidationDateAspect {
+    private final static Date VALIDATION_DATE;
+
+    static {
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(0);
+        cal.set(2018, 0, 1);
+        VALIDATION_DATE = cal.getTime();
+    }
+
+    @Around("call(void java.security.cert.X509Certificate.checkValidity()) && target(cert)")
+    public void aroundCheckValidity(X509Certificate cert) throws CertificateExpiredException, CertificateNotYetValidException {
+        cert.checkValidity(VALIDATION_DATE);
+    }
+}
